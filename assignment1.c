84d0103c79a3b456943d75411834aba2ab852e01
Tae Hyung Lee <teihyung@naver.com>
2023-01-30 20:33:47 -0800

    allowed english to be translated into bcitian

--- assignment1.c
+++ assignment1.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
 
-    char line[11];
+    char line[100];
 
     FILE *input;
     FILE *output;
@@ -35,10 +35,56 @@ int main(int argc, char **argv) {
             if (line[i] == '\0') {
                 break;
             }
-            fputc(line[i], output);
-            char_count++;
+            if (line[i] == 'I') {
+                fprintf(output, "Aei");
+
+            } else if (line[i] == 's' && line[i + 1] == 't' && line[i + 2] == 'u' && line[i + 3] == 'd' &&
+                       line[i + 4] == 'e' && line[i + 5] == 'n' && line[i + 6] == 't') {
+                fprintf(output, "worker");
+                i += 6;
+
+            } else if (line[i] == 'f' && line[i + 1] == 'r' && line[i + 2] == 'i' && line[i + 3] == 'e' &&
+                       line[i + 4] == 'n' && line[i + 5] == 'd') {
+                fprintf(output, "stranger");
+                i += 5;
+
+            } else if (line[i] == 'i' && line[i + 1] == 's') {
+                fprintf(output, "iizz");
+                i += 1;
+
+            } else if (line[i] == 'i' && line[i + 1] == 'n' && line[i + 2] == 'g') {
+                fprintf(output, "eeng");
+                i += 2;
+
+            } else if (line[i] == '.') {
+                fprintf(output, "[.]");
+                i += 2;
+
+            } else if (line[i] == 'e' && line[i + 1] == 'r') {
+                fprintf(output, "erre");
+                i += 2;
+
+            } else if (line[i] == 'p' && line[i + 1] == 'r' && line[i + 2] == 'o' && line[i + 3] == 'f' &&
+                       line[i + 4] == 'e' && line[i + 5] == 's' && line[i + 6] == 's' && line[i + 7] == 'o' &&
+                       line[i + 8] == 'r') {
+                fprintf(output, "king");
+                i+=8;
+            } else if (line[i] == 's' && line[i + 1] == 'k' && line[i + 2] == 'y' && line[i + 3] == 't' &&
+                       line[i + 4] == 'r' && line[i + 5] == 'a' && line[i + 6] == 'i' && line[i + 7] == 'n') {
+                fprintf(output, "sloth");
+                i += 7;
+            } else if (line[i] == 'c' && line[i + 1] == 'l' && line[i + 2] == 'a' && line[i + 3] == 's' &&
+                       line[i + 4] == 's') {
+                fprintf(output, "ccamp");
+
+            } else {
+                fputc(line[i], output);
+                char_count++;
+            }
+
         }
     }
     fclose(input);
     fclose(output);
+
 }
