779879509357a65616fcd0ca4523f5540215fad1
taehyuk <7374179@gmail.com>
2023-03-08 21:23:56 -0800

    first commit

--- lab6.c
+++ lab6.c
@@ -1,68 +1,107 @@
 #include <stdio.h>
 
-long long signed_min(int bitwidth){
-    //2^8 =256
-    // -128 ~ 127
-    long long tmp=1;
-    for(int i=0;i<bitwidth/2;i++){
-        tmp=tmp<<2;
+long long signed_min(int bitwidth) {
+    long long tmp = 1;
+    for (int i = 0; i < bitwidth / 2; i++) {
+        tmp = tmp << 2;
     }
-//    long long pos = tmp/2-1;
-    long long neg = tmp/2 *-1;
+    long long neg = tmp / 2 * -1;
     return neg;
 }
-long long signed_max(int bitwidth){
-    long long tmp=1;
-    for(int i=0;i<bitwidth/2;i++){
-        tmp=tmp<<2;
+
+long long signed_max(int bitwidth) {
+    long long tmp = 1;
+    for (int i = 0; i < bitwidth / 2; i++) {
+        tmp = tmp << 2;
     }
-    long long pos = tmp/2-1;
-//    long long neg = tmp/2 *-1;
+    long long pos = tmp / 2 - 1;
     return pos;
 }
-long long sat_add(long long operand1, long long operand2, int bitwidth){
-//    long long tmp=1;
-//    for(int i=0;i<bitwidth;i++){
-//        tmp=(operand1|operand2);
-//    }
-//    return tmp;
-    int c = 0;
-//        long long tmp=1;
-//    for(int i=0;i<bitwidth;i++){
-//        c = ((operand1 & operand2) <<1) ;
-//        operand1 = operand1 ^ operand2;
-//        operand2 = c;
-//        }
-//    return tmp;
 
-    while(operand2 != 0){
-        c = (((operand1 & operand2) & 0xff)<<1) ;
+long long sat_add(long long operand1, long long operand2, int bitwidth) {
+    int tmp = 2;
+    for (int i = 1; i < bitwidth; i++) {
+        tmp = tmp * 2;
+    }
+    tmp = tmp - 1;
+    int ttt = tmp/2;
+    int c = 0;
+
+    while (operand2 != 0) {
+        c = (((operand1 & operand2) & tmp) << 1);
         operand1 = operand1 ^ operand2;
         operand2 = c;
     }
+//    while (operand2 != 0) {
+//        c = (((operand1 & operand2) & 0xff) << 1);
+//        operand1 = operand1 ^ operand2;
+//        operand2 = c;
+//    }
 
-    return operand1;
+    return operand1 & (tmp/2);
 }
-long long sat_sub(long long operand1, long long operand2, int bitwidth){
+
+long long sat_sub(long long operand1, long long operand2, int bitwidth) {
+    int neg1 = 0;
+    int neg2 = 0;
+    int tmp = 2;
+    int c = 0;
+
+    for (int i = 1; i < bitwidth; i++) {
+        tmp = tmp * 2;
+    }
+    int tmp2 = tmp - 1;
+
+//    if (operand1 < 0 && operand2 > 0) {
+//        neg1 = tmp + operand1;
+//        neg2 = operand2;
+//        while (neg2 != 0) {
+//            c = (((neg1 & neg2) & tmp2) << 1);
+//            neg1 = neg1 ^ neg2;
+//            neg2 = c;
+//        }
+//        return neg1 - tmp;
+//    } else if (operand1 > 0 && operand2 < 0) {
+//        neg1 = operand1;
+//        neg2 = tmp + operand2;
+//        while (neg2 != 0) {
+//            c = (((neg1 & neg2) & tmp2) << 1);
+//            neg1 = neg1 ^ neg2;
+//            neg2 = c;
+//        }
+//        return neg1 - tmp;
+//    } else if (operand1 < 0 && operand2 < 0) {
+//        neg1 = tmp + operand1;
+//        neg2 = tmp + operand2;
+//        while (neg2 != 0) {
+//            c = (((neg1 & neg2) & tmp2) << 1);
+//            neg1 = neg1 ^ neg2;
+//            neg2 = c;
+//        }
+//        return neg1 - tmp*2;
+//    }
+
+    neg1 = tmp + operand1;
+    neg2 = tmp + operand2;
+    while (neg2 != 0) {
+        c = (((neg1 & neg2) & tmp2) << 1);
+        neg1 = neg1 ^ neg2;
+        neg2 = c;
+    }
+    return neg1 - tmp * 2;
 
 }
 
-//long long tr(int bitwidth){
-//
-//}
 
 int main() {
-//    long long signed_min(int bitwidth);
     long long a = signed_min(8);
-//    long long signed_max(int bitwidth);
     long long b = signed_max(8);
-//    long long sat_add(long long operand1, long long operand2, int bitwidth);
     int c = sat_add(125, 5, 8);
-    long long sat_sub(long long operand1, long long operand2, int bitwidth);
-//    long long tr(8);
+    int d = sat_sub(125, -22, 8);
 
     printf("min: %d\t0x%016llx\n", a, a);
     printf("max: %d\t0x%016llx\n", b, b);
-    printf("%d", c);
+    printf("%d\n", c);
+    printf("%d\n", d);
 
 }
