1b4e85cb8b13ba6b51b4a5da5934d13dad6ff195
taehyuk <7374179@gmail.com>
2023-03-10 16:44:42 -0800

    complete

--- lab6.c
+++ lab6.c
@@ -1,107 +1,65 @@
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
 
 long long signed_min(int bitwidth) {
-    long long tmp = 1;
-    for (int i = 0; i < bitwidth / 2; i++) {
-        tmp = tmp << 2;
-    }
-    long long neg = tmp / 2 * -1;
-    return neg;
+    return LLONG_MIN >> (64 - bitwidth);
 }
 
 long long signed_max(int bitwidth) {
-    long long tmp = 1;
-    for (int i = 0; i < bitwidth / 2; i++) {
-        tmp = tmp << 2;
-    }
-    long long pos = tmp / 2 - 1;
-    return pos;
+    return LLONG_MAX >> (64 - bitwidth);
 }
 
 long long sat_add(long long operand1, long long operand2, int bitwidth) {
-    int tmp = 2;
-    for (int i = 1; i < bitwidth; i++) {
-        tmp = tmp * 2;
-    }
-    tmp = tmp - 1;
-    int ttt = tmp/2;
-    int c = 0;
+    long long max_val = signed_max(bitwidth);
+    long long min_val = signed_min(bitwidth);
+    long long result = operand1 + operand2;
+    int sign_bit = (result >> (bitwidth - 1)) & 0x01;
+    int negative_bit = (result >> (bitwidth)) & 0x01;
 
-    while (operand2 != 0) {
-        c = (((operand1 & operand2) & tmp) << 1);
-        operand1 = operand1 ^ operand2;
-        operand2 = c;
+    if (sign_bit == 1 && negative_bit == 0) {
+        return max_val;
+    } else if (negative_bit == 1 && sign_bit == 0) {
+        return min_val;
+    } else {
+        return result;
     }
-//    while (operand2 != 0) {
-//        c = (((operand1 & operand2) & 0xff) << 1);
-//        operand1 = operand1 ^ operand2;
-//        operand2 = c;
-//    }
-
-    return operand1 & (tmp/2);
 }
+
 
 long long sat_sub(long long operand1, long long operand2, int bitwidth) {
-    int neg1 = 0;
-    int neg2 = 0;
-    int tmp = 2;
-    int c = 0;
+    long long max_val = signed_max(bitwidth);
+    long long min_val = signed_min(bitwidth);
+    long long result = operand1 - operand2;
 
-    for (int i = 1; i < bitwidth; i++) {
-        tmp = tmp * 2;
+    int sign_bit = (result >> (bitwidth - 1)) & 0x01;
+    int negative_bit = (result >> (bitwidth)) & 0x01;
+    if (sign_bit == 1 && negative_bit == 0) {
+        return max_val;
+    } else if (negative_bit == 1 && sign_bit == 0) {
+        return min_val;
+    } else {
+        return result;
     }
-    int tmp2 = tmp - 1;
-
-//    if (operand1 < 0 && operand2 > 0) {
-//        neg1 = tmp + operand1;
-//        neg2 = operand2;
-//        while (neg2 != 0) {
-//            c = (((neg1 & neg2) & tmp2) << 1);
-//            neg1 = neg1 ^ neg2;
-//            neg2 = c;
-//        }
-//        return neg1 - tmp;
-//    } else if (operand1 > 0 && operand2 < 0) {
-//        neg1 = operand1;
-//        neg2 = tmp + operand2;
-//        while (neg2 != 0) {
-//            c = (((neg1 & neg2) & tmp2) << 1);
-//            neg1 = neg1 ^ neg2;
-//            neg2 = c;
-//        }
-//        return neg1 - tmp;
-//    } else if (operand1 < 0 && operand2 < 0) {
-//        neg1 = tmp + operand1;
-//        neg2 = tmp + operand2;
-//        while (neg2 != 0) {
-//            c = (((neg1 & neg2) & tmp2) << 1);
-//            neg1 = neg1 ^ neg2;
-//            neg2 = c;
-//        }
-//        return neg1 - tmp*2;
-//    }
-
-    neg1 = tmp + operand1;
-    neg2 = tmp + operand2;
-    while (neg2 != 0) {
-        c = (((neg1 & neg2) & tmp2) << 1);
-        neg1 = neg1 ^ neg2;
-        neg2 = c;
-    }
-    return neg1 - tmp * 2;
 
 }
 
 
-int main() {
-    long long a = signed_min(8);
-    long long b = signed_max(8);
-    int c = sat_add(125, 5, 8);
-    int d = sat_sub(125, -22, 8);
-
-    printf("min: %d\t0x%016llx\n", a, a);
-    printf("max: %d\t0x%016llx\n", b, b);
-    printf("%d\n", c);
-    printf("%d\n", d);
-
+int main(int argc, char **argv) {
+    int bit = atoi(argv[1]);
+    if (argc == 2) {
+        printf("%d-bit signed integer range\n", bit);
+        printf("min: %lld\t0x%016llx\n", signed_min(bit), signed_min(bit));
+        printf("max: %lld\t0x%016llx", signed_max(bit), signed_max(bit));
+    } else {
+        long long op1 = atoi(argv[2]);
+        long long op2 = atoi(argv[3]);
+        if (atoi(argv[4]) == 0) {
+            printf("%s + %s = %lld", argv[2], argv[3], sat_add(op1, op2, bit));
+        } else {
+            printf("%s - %s = %lld", argv[2], argv[3], sat_sub(op1, op2, bit));
+        }
+    }
+    return 0;
 }
