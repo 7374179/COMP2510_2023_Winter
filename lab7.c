8c455b6cf551b1a49e553e7185993ded7511f435
Taehyuk <7374179@gmail.com>
2023-03-21 22:55:00 -0700

    gogo

---
+++ lab7.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+/**
+   DO NOT CHANGE
+   1st sample struct containing 2 integers
+   When sorting use x first, then y.
+**/
+typedef struct{
+  int x;
+  int y;
+}istr;
+
+/**
+   DO NOT CHANGE
+   2nd sample struct containing 3 characters
+   When sorting use x first, then y, and finally z. 
+**/
+typedef struct{
+  char x;
+  char y;
+  char z;
+}cstr;
+
+/**
+   The function takes the pointer to linked list and
+   a pointer to data.
+   This function adds data at the end of your linked list
+   However, the return value is 1 if successfully inserted
+   0 otherwise
+**/
+int add_node(void *ll, void *data){
+  return 1;
+}
+
+/**
+   Remove an element at the end of the linked list
+   The function takes the pointer to linked list and
+   a pointer to return value (removed data)
+**/
+void remove_node(void *ll, void *ret){
+  return;
+}
+
+/**
+   This function creates a linked list. This function returns
+   the linked list. Returns NULL if creation fails.
+   You are required to make your own custom linked list struct
+ **/
+void *create_ll(size_t size){
+  return NULL;
+}
+
+/**
+   This function merge sorts the linked list.
+ **/
+void sort(void *ll){
+  return;
+}
+
+/**
+   Do not modify the function signature of the main function
+   including spacing.
+ **/
+int main(int argc, char** argv){ // Basically do NOT touch this line
+  /**
+     sample int ll code
+     ll* ill = create_ll(sizeof(int));
+     int i = 0;
+     add(ill, &i);
+     i += 1;
+     add(ill, &i);
+     sort(ill);
+     int rv
+     remove(ill, $&v);
+     printf("%d", rv);
+  **/
+
+  
+  return 0;
+}
