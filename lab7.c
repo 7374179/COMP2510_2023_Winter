db611ce5fcd792efc04090374295af43d0ac2881
Taehyuk <7374179@gmail.com>
2023-03-22 01:34:50 -0700

    done

--- lab7.c
+++ lab7.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <string.h>
 
 /**
    DO NOT CHANGE
@@ -28,8 +30,33 @@ typedef struct{
    However, the return value is 1 if successfully inserted
    0 otherwise
 **/
+typedef struct node {
+    void *data;
+    struct node *next;
+} Node;
+typedef struct {
+    Node *head;
+    Node *crnt;
+    size_t data_size;
+
+} List;
 int add_node(void *ll, void *data){
-  return 1;
+    List* list = (List*) ll;
+    Node* new_node = malloc(sizeof(Node));
+    new_node->data = malloc(list->data_size);
+    memcpy(new_node->data, data, list->data_size);
+    new_node->next = NULL;
+
+    if (list->head == NULL) {
+        list->head = new_node;
+    } else {
+        Node* curr = list->head;
+        while (curr->next != NULL) {
+            curr = curr->next;
+        }
+        curr->next = new_node;
+    }
+    return 1;
 }
 
 /**
@@ -38,7 +65,24 @@ int add_node(void *ll, void *data){
    a pointer to return value (removed data)
 **/
 void remove_node(void *ll, void *ret){
-  return;
+//    if (((ll*)ll)->head == NULL) {
+//        return 0;
+//    }
+    Node *curr = ((List*)ll)->head;
+    Node *prev = NULL;
+    while (curr->next != NULL) {
+        prev = curr;
+        curr = curr->next;
+    }
+    memcpy(ret, curr->data, ((List*)ll)->data_size);
+    if (prev == NULL) {
+        ((List*)ll)->head = NULL;
+    } else {
+        prev->next = NULL;
+    }
+    free(curr->data);
+    free(curr);
+    return;
 }
 
 /**
@@ -47,15 +91,23 @@ void remove_node(void *ll, void *ret){
    You are required to make your own custom linked list struct
  **/
 void *create_ll(size_t size){
-  return NULL;
+    List *new_ll = malloc(sizeof(List));
+    if (new_ll == NULL) {
+        return NULL; // If memory allocation fails, return NULL
+    }
+    new_ll->head = NULL;
+    new_ll->data_size = size;
+    return (void*)new_ll;
+//  return NULL;
 }
 
 /**
    This function merge sorts the linked list.
  **/
 void sort(void *ll){
-  return;
-}
+    if(((List*)ll)->head == NULL || ((List*)ll)->head->next == NULL) {
+        return;
+    }}
 
 /**
    Do not modify the function signature of the main function
@@ -74,6 +126,29 @@ int main(int argc, char** argv){ // Basically do NOT touch this line
      remove(ill, $&v);
      printf("%d", rv);
   **/
+
+//    List* ill = create_ll(sizeof(int));
+//    int i = 0;
+//    add_node(ill, &i);
+//    i += 1;
+//    add_node(ill, &i);
+//    sort(ill);
+//    int rv;
+//    remove_node(ill, &rv);
+//    printf("%d\n", rv);
+
+    List* ill = create_ll(sizeof(int));
+    char i = 'a';
+    add_node(ill, &i);
+    i += 1;
+    add_node(ill, &i);
+    i += 1;
+    add_node(ill, &i);
+    sort(ill);
+    int rv;
+    remove_node(ill, &rv);
+    printf("%c\n", rv);
+
 
   
   return 0;
