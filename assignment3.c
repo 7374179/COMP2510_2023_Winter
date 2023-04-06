75255e4b71b65c3db875b55f2da85c3453aaded2
TroyCalaquian <trcalaquian@gmail.com>
2023-04-05 19:47:01 -0700

    Corner case testing

--- assignment3.c
+++ assignment3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct bnode {
+typedef struct bnode
+{
     int data;
     struct bnode *left;
     struct bnode *right;
@@ -11,41 +12,58 @@ int Remove(BinNode **root, int data);
 BinNode *Add1(BinNode *p, int data);
 BinNode *Add2(BinNode *p, int data);
 
-static BinNode *AllocBinNode(void) {
+static BinNode *AllocBinNode(void)
+{
     return calloc(1, sizeof(BinNode));
 }
 
-static void SetBinNode(BinNode *n, int data, BinNode *left, BinNode *right) {
+static void SetBinNode(BinNode *n, int data, BinNode *left, BinNode *right)
+{
     n->data = data;
     n->left = left;
     n->right = right;
 }
 
-int Remove(BinNode **root, int data) {
+int Remove(BinNode **root, int data)
+{
     BinNode *next, *temp;
     BinNode **left;
     BinNode **p = root;
 
-    while (1) {
-        if (*p == NULL) {
-            printf("Error! %d is not here\n", data);
+    while (1)
+    {
+        if (*p == NULL)
+        {
+            printf("COMP2510ERROR: %d is not here\n", data);
             return -1;
-        } else if (data == (*p)->data) {
+        }
+        else if (data == (*p)->data)
+        {
             break;
-        } else if (data < (*p)->data) {
+        }
+        else if (data < (*p)->data)
+        {
             p = &((*p)->left);
-        } else if (data > (*p)->data) {
+        }
+        else if (data > (*p)->data)
+        {
             p = &((*p)->right);
-        } else{
+        }
+        else
+        {
             *p = Add2(*p, (*p)->data);
         }
     }
 
-    if ((*p)->left == NULL) {
+    if ((*p)->left == NULL)
+    {
         next = (*p)->right;
-    } else {
+    }
+    else
+    {
         left = &((*p)->left);
-        while ((*left)->right != NULL){
+        while ((*left)->right != NULL)
+        {
             left = &(*left)->right;
         }
         next = *left;
@@ -60,50 +78,68 @@ int Remove(BinNode **root, int data) {
     return 0;
 }
 
-BinNode *Add1(BinNode *p, int data) {
-    if (p == NULL) {
+BinNode *Add1(BinNode *p, int data)
+{
+    if (p == NULL)
+    {
         p = AllocBinNode();
         SetBinNode(p, data, NULL, NULL);
     }
-    else if (data == p->data) {
+    else if (data == p->data)
+    {
         p->left = Add1(p->left, data);
-    } else if (data < p->data) {
-//        Add(p->left, data);
+    }
+    else if (data < p->data)
+    {
+        //        Add(p->left, data);
         p->left = Add1(p->left, data);
-    } else if (data > p->data){
-//        Add(p->right, data);
+    }
+    else if (data > p->data)
+    {
+        //        Add(p->right, data);
         p->right = Add1(p->right, data);
     }
     return p;
 }
-BinNode *Add2(BinNode *p, int data) {
-    if (p == NULL) {
+BinNode *Add2(BinNode *p, int data)
+{
+    if (p == NULL)
+    {
         p = AllocBinNode();
         SetBinNode(p, data, NULL, NULL);
     }
-    else if (data == p->data) {
-//        printf("Error! There is same data\n");
+    else if (data == p->data)
+    {
+        //        printf("Error! There is same data\n");
         Remove(&p, p->data);
-    } else if (data < p->data) {
-//        Add(p->left, data);
+    }
+    else if (data < p->data)
+    {
+        //        Add(p->left, data);
         p->left = Add2(p->left, data);
-    } else if (data > p->data){
-//        Add(p->right, data);
+    }
+    else if (data > p->data)
+    {
+        //        Add(p->right, data);
         p->right = Add2(p->right, data);
     }
     return p;
 }
 
-void PrintTree(FILE *fop, const BinNode *p) {
-    if (p != NULL) {
+void PrintTree(FILE *fop, const BinNode *p)
+{
+    if (p != NULL)
+    {
         fprintf(fop, "%d\n", p->data);
         PrintTree(fop, p->left);
         PrintTree(fop, p->right);
     }
 }
 
-void freeTree(BinNode* root){
-    if(root == NULL){
+void freeTree(BinNode *root)
+{
+    if (root == NULL)
+    {
         return;
     }
     freeTree(root->left);
@@ -111,25 +147,43 @@ void freeTree(BinNode* root){
     free(root);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     FILE *fip1, *fip2, *fop;
     int data;
     BinNode *root = NULL;
+    char line[10];
 
     fip1 = fopen(argv[1], "r");
     fip2 = fopen(argv[2], "r");
     fop = fopen(argv[3], "w");
 
-    if (fip1 == NULL || fip2 == NULL || fop == NULL) {
-        printf("Failed to open file\n");
+    if (fip1 == NULL || fip2 == NULL || fop == NULL)
+    {
+        printf("COMP2510ERROR: Failed to open file\n");
     }
 
-    while (fscanf(fip1, "%d", &data) != EOF) {
-        root = Add1(root, data);
+while (fgets(line, sizeof(line), fip1) != NULL)
+{
+    if (sscanf(line, "%d%*[^ \t\n]%*[ \t\n]", &data) != 1)
+    {
+        printf("COMP2510ERROR: Invalid input format\n");
+        exit(1);
     }
-    while (fscanf(fip2, "%d", &data) != EOF) {
-        root = Add2(root, data);
+    root = Add1(root, data);
+}
+
+while (fgets(line, sizeof(line), fip2) != NULL)
+{
+    if (sscanf(line, "%d%*[^ \t\n]%*[ \t\n]", &data) != 1)
+    {
+        printf("COMP2510: Invalid input format\n");
+        exit(1);
     }
+    root = Add2(root, data);
+}
+
+
     PrintTree(fop, root);
 
     fclose(fip1);
@@ -140,4 +194,3 @@ int main(int argc, char **argv) {
 
     return 0;
 }
-
